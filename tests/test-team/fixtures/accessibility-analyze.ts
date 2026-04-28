b5309ad5bf977ab59f9ee7627812fa7111d6ee30
alex.nuttall1 <alex.nuttall1@nhs.net>
2026-04-28 13:00:49 +0100

    add warnings

--- tests/test-team/fixtures/accessibility-analyze.ts
+++ tests/test-team/fixtures/accessibility-analyze.ts
@@ -13,17 +13,17 @@ type AccessibilityFixture = {
   analyze: Analyze;
 };
 
-const DISABLED_RULES = [
+const WARN_LEVEL_RULES = new Set([
   /* We don't have control over NHS / CIS2 colours. */
   'color-contrast-enhanced',
-];
+]);
 
 const DEFAULT_REDIRECT_PATH = '/templates/message-templates';
 
 const makeAxeBuilder = (page: Page) =>
-  new AxeBuilder({ page })
-    .withTags(['wcag2a', 'wcag2aa', 'wcag2aaa'])
-    .disableRules(DISABLED_RULES);
+  new AxeBuilder({ page }).withTags(['wcag2a', 'wcag2aa', 'wcag2aaa']);
+// .disableRules(DISABLED_RULES);
+// .options({ rules: { 'color-contrast-enhanced': {} } });
 
 export const test = base.extend<AccessibilityFixture>({
   analyze: async ({ page }, use) => {
@@ -42,7 +42,24 @@ export const test = base.extend<AccessibilityFixture>({
 
       const results = await makeAxeBuilder(page).analyze();
 
-      expect(results.violations).toEqual([]);
+      const failures = results.violations.filter((violation) => {
+        if (WARN_LEVEL_RULES.has(violation.id)) {
+          const warning = JSON.stringify(violation, null, 2);
+
+          // eslint-disable-next-line no-console
+          console.warn(`WARNING: ${warning}`);
+
+          test.info().annotations.push({
+            type: 'Warning',
+            description: warning,
+          });
+
+          return false;
+        }
+        return true;
+      });
+
+      expect(failures).toEqual([]);
     };
 
     // eslint-disable-next-line react-hooks/rules-of-hooks
